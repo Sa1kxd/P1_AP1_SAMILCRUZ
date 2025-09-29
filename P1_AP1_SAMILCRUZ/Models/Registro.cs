7726fa039041e022bd4c2813e7bae397b7c02011
Sa1kxd <samildelacruz1@gmail.com>
2025-09-29 09:09:54 -0400

    Modelo creado

---
+++ P1_AP1_SAMILCRUZ/Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_SAMILCRUZ.Models;
+
+public class Registro
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nombre es requerido")]
+    public string Nombres { get; set; }
+}
