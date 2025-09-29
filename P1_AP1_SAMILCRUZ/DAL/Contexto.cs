9e0dc22de0214cb7fdf0886b7632b34562b1610e
Sa1kxd <samildelacruz1@gmail.com>
2025-09-29 09:11:24 -0400

    Contexto creado

---
+++ P1_AP1_SAMILCRUZ/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_SAMILCRUZ.Models;
+
+namespace P1_AP1_SAMILCRUZ.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    DbSet<Registro> Registros { get; set; }
+
+}
