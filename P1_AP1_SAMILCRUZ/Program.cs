39438589bf6d56f4bfac5435c520dcefdf87d8c7
Sa1kxd <samildelacruz1@gmail.com>
2025-09-29 09:16:41 -0400

    Inyeccion del contexto

--- P1_AP1_SAMILCRUZ/Program.cs
+++ P1_AP1_SAMILCRUZ/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using P1_AP1_SAMILCRUZ.Components;
+using P1_AP1_SAMILCRUZ.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var Constr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(options => options.UseSqlite(Constr));
 
 var app = builder.Build();
 
